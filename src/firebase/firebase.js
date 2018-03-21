db4655d03ea1966716df7f169495fb15256b1292
Jonathan Sheldrake <jonathan.sheldrake@uk.ibm.com>
2018-03-21 14:43:25 +0000

    Setup test database environment

---
+++ src/firebase/firebase.js
@@ -0,0 +1,101 @@
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// // child_removed
+// const onChildRemoved = database.ref('expenses')
+// .on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_changed
+// const onChildChanged = database.ref('expenses')
+// .on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// const onChildAdded = database.ref('expenses')
+// .on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// const expenses = [{
+//   description: 'rent',
+//   note: 'home rent',
+//   amount: '100000',
+//   createdAt: (new Date).getTime(),
+// }, {
+//   description: 'electricity',
+//   note: 'electricity bill',
+//   amount: '5000',
+//   createdAt: (new Date).getTime(),
+// }, {
+//   description: 'gas',
+//   note: 'gas bill',
+//   amount: '4000',
+//   createdAt: (new Date).getTime(),
+// }]
+
+// expenses.forEach(expense => database.ref('expenses').push(expense));
+
+// database.ref('notes').push({
+//   title: 'course topics',
+//   body: 'react native, angular, python',
+// });
+
+// database.ref('notes').set(notes);
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// }, (e) => {
+//   console.log('Error with data fetching', e);
+// });
+
+// database.ref('value')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e)
+//   });
+
+// database.ref().set({
+//   name: 'Jon Sheldrake',
+//   age: '36',
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Philadephia',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+  
