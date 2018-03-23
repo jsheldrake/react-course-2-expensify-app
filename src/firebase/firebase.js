3cb03da56ee7b8556ed1c50c43345b163d164866
Jonathan Sheldrake <jonathan.sheldrake@uk.ibm.com>
2018-03-23 16:45:41 +0000

    add google authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,8 +11,9 @@ var config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // // child_removed
 // const onChildRemoved = database.ref('expenses')
